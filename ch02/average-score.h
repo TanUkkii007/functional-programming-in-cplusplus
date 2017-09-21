a27cfd5c6ff363cfd1d5b4dab8c61915ae5dfdd7
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2017-09-21 23:17:09 +0900

    add score_product function

--- ch02/average-score.h
+++ ch02/average-score.h
@@ -1,9 +1,19 @@
 #include <vector>
 #include <numeric>
+#include <functional>
 
 double average_score(const std::vector<int> &scores)
 {
     return std::accumulate(
         scores.cbegin(), scores.cend(), 0
     ) / (double) scores.size();
+}
+
+double scores_product(const std::vector<int> &scores)
+{
+    return std::accumulate(
+            scores.cbegin(), scores.cend(),
+            1,
+            std::multiplies<int>()
+    );
 }
